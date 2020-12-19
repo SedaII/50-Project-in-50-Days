07fd9a4fb9ec68b601795b5cc91232b3ba4644a6
SedaII <i.messnaoui@gmail.com>
2020-12-19 19:40:14 +0100

    Day 17 - Movie App - Movies UI Layout

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,3 +1,29 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=34ffbc41f61cd831dd85524b41b1462f&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
-const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?&api_key=34ffbc41f61cd831dd85524b41b1462f&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?&api_key=34ffbc41f61cd831dd85524b41b1462f&query="'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+//Get initial Movies
+
+getMovies(API_URL)
+
+async function getMovies(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  console.log(data.results)
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const searchTerm = search.value
+  if(searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm)
+
+    search.value = ''
+  } else {
+    window.location.reload()
+  }
+})
